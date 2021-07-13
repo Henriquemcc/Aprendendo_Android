ee673c6d1c38c464ce86637185923e8edcedce25
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-07-13 17:04:17 -0300

    Parte 4 da aula 2 do curso Android parte 1: Crie a sua primeira app mobile.

--- Agenda/app/src/main/java/io/github/henriquemcc/agenda/MainActivity.kt
+++ Agenda/app/src/main/java/io/github/henriquemcc/agenda/MainActivity.kt
@@ -2,14 +2,20 @@ package io.github.henriquemcc.agenda
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Henrique", Toast.LENGTH_LONG).show()
         setContentView(R.layout.activity_main)
+        val alunos = arrayListOf("Alex", "José", "Fran")
+        val primeiroAluno = findViewById<TextView>(R.id.textView)
+        val segundoAluno = findViewById<TextView>(R.id.textView2)
+        val terceiroAluno = findViewById<TextView>(R.id.textView3)
+        primeiroAluno.text = alunos[0]
+        segundoAluno.text = alunos[1]
+        terceiroAluno.text = alunos[2]
     }
 
 }
