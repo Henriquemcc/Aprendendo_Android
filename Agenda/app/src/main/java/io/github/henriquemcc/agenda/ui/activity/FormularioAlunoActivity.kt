6da6da3c8e345fc75fc0f653a2f1f07bdd35a00c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-07-14 17:36:02 -0300

    Parte 7 da aula 5 do curso Android parte 1: Crie a sua primeira app mobile.

--- Agenda/app/src/main/java/io/github/henriquemcc/agenda/ui/activity/FormularioAlunoActivity.kt
+++ Agenda/app/src/main/java/io/github/henriquemcc/agenda/ui/activity/FormularioAlunoActivity.kt
@@ -12,6 +12,7 @@ class FormularioAlunoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_aluno)
+        title = "Novo aluno"
 
         val dataAccessObjectAluno = AlunoDataAccessObject()
 
