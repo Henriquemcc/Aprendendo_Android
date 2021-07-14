8baaae16867a82c459927af9fbeb1d94d4c848e9
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-07-14 09:12:18 -0300

    Parte 4 da aula 4 do curso Android parte 1: Crie a sua primeira app mobile.

--- Agenda/app/src/main/java/io/github/henriquemcc/agenda/MainActivity.kt
+++ Agenda/app/src/main/java/io/github/henriquemcc/agenda/MainActivity.kt
@@ -1,15 +1,16 @@
 package io.github.henriquemcc.agenda
 
-import android.app.Activity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Lista de alunos"
         val alunos = arrayListOf("Alex", "José", "Fran", "Maria", "Ana")
         val listaDeAlunos = findViewById<ListView>(R.id.activity_main_lista_de_alunos)
         listaDeAlunos.adapter =
