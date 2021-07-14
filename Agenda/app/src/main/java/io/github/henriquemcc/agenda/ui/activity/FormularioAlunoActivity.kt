510db71e36edf85bd2b0ad57b6aa542b302f0fe3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-07-14 09:46:40 -0300

    Parte 6 e 7 da aula 4 do curso Android parte 1: Crie a sua primeira app mobile.

---
+++ Agenda/app/src/main/java/io/github/henriquemcc/agenda/ui/activity/FormularioAlunoActivity.kt
@@ -0,0 +1,12 @@
+package io.github.henriquemcc.agenda.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.henriquemcc.agenda.R
+
+class FormularioAlunoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_aluno)
+    }
+}
