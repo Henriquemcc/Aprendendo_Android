0525783ecf8d5a74d44786da1cae13422750f4b9
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-07-14 09:46:40 -0300

    Parte 6 da aula 4 do curso Android parte 1: Crie a sua primeira app mobile.

--- Agenda/app/src/main/java/io/github/henriquemcc/agenda/ui/activity/ListaAlunosActivity.kt
+++ Agenda/app/src/main/java/io/github/henriquemcc/agenda/ui/activity/ListaAlunosActivity.kt
@@ -1,18 +1,19 @@
-package io.github.henriquemcc.agenda
+package io.github.henriquemcc.agenda.ui.activity
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import io.github.henriquemcc.agenda.R
 
-class MainActivity : AppCompatActivity() {
+class ListaAlunosActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lista_alunos)
         title = "Lista de alunos"
         val alunos = arrayListOf("Alex", "José", "Fran", "Maria", "Ana")
-        val listaDeAlunos = findViewById<ListView>(R.id.activity_main_lista_de_alunos)
+        val listaDeAlunos = findViewById<ListView>(R.id.activity_lista_de_alunos_listview)
         listaDeAlunos.adapter =
             ArrayAdapter(this, android.R.layout.simple_list_item_1, alunos)
     }
