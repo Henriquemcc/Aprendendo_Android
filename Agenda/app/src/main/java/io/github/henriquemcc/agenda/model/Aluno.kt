14f7d7c624e6e666fb64ff3f1ed7efb528f4fd5c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-07-14 12:57:45 -0300

    Parte 2 da aula 5 do curso Android parte 1: Crie a sua primeira app mobile.

---
+++ Agenda/app/src/main/java/io/github/henriquemcc/agenda/model/Aluno.kt
@@ -0,0 +1,3 @@
+package io.github.henriquemcc.agenda.model
+
+class Aluno(val nome: String, val telefone: String, val email: String)
--- Agenda/app/src/main/java/io/github/henriquemcc/agenda/ui/activity/FormularioAlunoActivity.kt
+++ Agenda/app/src/main/java/io/github/henriquemcc/agenda/ui/activity/FormularioAlunoActivity.kt
@@ -1,12 +1,34 @@
 package io.github.henriquemcc.agenda.ui.activity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.github.henriquemcc.agenda.R
+import io.github.henriquemcc.agenda.model.Aluno
 
 class FormularioAlunoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_aluno)
+
+        val campoNome = findViewById<EditText>(R.id.activity_formulario_aluno_nome)
+        val campoTelefone = findViewById<EditText>(R.id.activity_formulario_aluno_telefone)
+        val campoEmail = findViewById<EditText>(R.id.activity_formulario_aluno_email)
+
+        val botaoSalvar = findViewById<Button>(R.id.activity_formulario_aluno_botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val nome = campoNome.text.toString()
+            val telefone = campoTelefone.text.toString()
+            val email = campoEmail.text.toString()
+
+            val alunoCriado = Aluno(nome, telefone, email)
+            Toast.makeText(
+                this,
+                "${alunoCriado.nome} - ${alunoCriado.telefone} - ${alunoCriado.email}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
