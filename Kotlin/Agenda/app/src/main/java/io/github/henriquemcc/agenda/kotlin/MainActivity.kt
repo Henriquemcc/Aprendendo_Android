283d0edd7c7b918721420f23219ed4607dbd7d3f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-06 17:32:24 -0300

    Parte 13 da aula 1 do curso 'Android parte 1: crie um app mobile' em Kotlin.

--- Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/MainActivity.kt
+++ Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/MainActivity.kt
@@ -1,8 +1,14 @@
 package io.github.henriquemcc.agenda.kotlin
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity: Activity()
 {
-
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Henrique", Toast.LENGTH_LONG).show()
+    }
 }
