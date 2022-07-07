d0a722346571f4a84168b7ba5033c58ee27d6d6e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-06 22:51:50 -0300

    Parte 6 da aula 2 do curso 'Android parte 1: crie um app mobile' em Kotlin.

--- Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/MainActivity.kt
+++ Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/MainActivity.kt
@@ -2,6 +2,8 @@ package io.github.henriquemcc.agenda.kotlin
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,12 +13,14 @@ class MainActivity: Activity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val alunos = listOf<String>("João", "José", "Maria")
-        val primeiroAluno = findViewById<TextView>(R.id.textView0)
-        primeiroAluno.text = alunos[0]
-        val segundoAluno = findViewById<TextView>(R.id.textView1)
-        segundoAluno.text = alunos[1]
-        val terceiroAluno = findViewById<TextView>(R.id.textView2)
-        terceiroAluno.text = alunos[2]
+
+        // Lista de alunos
+        val alunos = listOf<String>("João", "José", "Maria", "Carlos", "Gabriel", "Ana")
+
+        // List View com os alunos
+        val listViewListaDeAlunos = findViewById<ListView>(R.id.activity_main_lista_de_alunos)
+
+        // Adicionando adaptador com uma lista simples de itens
+        listViewListaDeAlunos.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, alunos)
     }
 }
