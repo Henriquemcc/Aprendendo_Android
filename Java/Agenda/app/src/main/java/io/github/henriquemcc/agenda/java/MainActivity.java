504c43bb9d636b818abd506d7be4789468f609c1
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-06 21:45:16 -0300

    Parte 2 da aula 2 do curso 'Android parte 1: crie um app mobile' em Java.

--- Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/MainActivity.java
+++ Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/MainActivity.java
@@ -2,7 +2,6 @@ package io.github.henriquemcc.agenda.java;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity
@@ -11,6 +10,6 @@ public class MainActivity extends Activity
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Henrique", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
     }
 }
