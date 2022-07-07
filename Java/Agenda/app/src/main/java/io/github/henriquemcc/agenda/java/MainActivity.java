7047c60fc0be32481b739174984e7d1129996975
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-06 22:12:37 -0300

    Parte 4 da aula 2 do curso 'Android parte 1: crie um app mobile' em Java.

--- Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/MainActivity.java
+++ Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/MainActivity.java
@@ -2,7 +2,12 @@ package io.github.henriquemcc.agenda.java;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +16,16 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Lista de alunos
+        final List<String> alunos = Arrays.asList("João", "José", "Maria");
+
+        // Adicionando string dos alunos aos text views
+        final TextView primeiroAluno = findViewById(R.id.textView0);
+        primeiroAluno.setText(alunos.get(0));
+        final TextView segundoAluno = findViewById(R.id.textView1);
+        segundoAluno.setText(alunos.get(1));
+        final TextView terceiroAluno = findViewById(R.id.textView2);
+        terceiroAluno.setText(alunos.get(2));
     }
 }
