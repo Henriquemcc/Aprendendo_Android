c71acba9987f5296087956bbf8f47edf7ee61ae1
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-06 22:18:22 -0300

    Parte 4 da aula 2 do curso 'Android parte 1: crie um app mobile' em Kotlin.

--- Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/MainActivity.kt
+++ Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package io.github.henriquemcc.agenda.kotlin
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity: Activity()
@@ -10,5 +11,12 @@ class MainActivity: Activity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val alunos = listOf<String>("João", "José", "Maria")
+        val primeiroAluno = findViewById<TextView>(R.id.textView0)
+        primeiroAluno.text = alunos[0]
+        val segundoAluno = findViewById<TextView>(R.id.textView1)
+        segundoAluno.text = alunos[1]
+        val terceiroAluno = findViewById<TextView>(R.id.textView2)
+        terceiroAluno.text = alunos[2]
     }
 }
