552c441b26fc637d46b15fdc720cc07a9f0042db
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-19 20:57:13 -0300

    Parte 6 da aula 4 do curso 'Android parte 1: crie um app mobile'.

---
+++ Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/ui/activity/FormularioAlunoActivity.kt
@@ -0,0 +1,12 @@
+package io.github.henriquemcc.agenda.kotlin.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.henriquemcc.agenda.kotlin.R
+
+class FormularioAlunoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_aluno)
+    }
+}
