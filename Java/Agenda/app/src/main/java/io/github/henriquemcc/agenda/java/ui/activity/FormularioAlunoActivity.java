c0f154cf7d0c575266f77b1b074ba085d6e1abca
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-20 16:30:47 -0300

    Parte 2 da aula 5 do curso 'Android parte 1: crie um app mobile' em Java.

--- Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/ui/activity/FormularioAlunoActivity.java
+++ Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/ui/activity/FormularioAlunoActivity.java
@@ -3,14 +3,43 @@ package io.github.henriquemcc.agenda.java.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import io.github.henriquemcc.agenda.java.R;
+import io.github.henriquemcc.agenda.java.model.Aluno;
 
-public class FormularioAlunoActivity extends AppCompatActivity {
+public class FormularioAlunoActivity extends AppCompatActivity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario_aluno);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_formulario_aluno);
+
+		final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+		final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+		final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+		final Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+		botaoSalvar.setOnClickListener(
+				new View.OnClickListener()
+				{
+
+					@Override
+					public void onClick(View view)
+					{
+						final String nome = campoNome.getText().toString();
+						final String telefone = campoTelefone.getText().toString();
+						final String email = campoEmail.getText().toString();
+
+						Aluno alunoCriado = new Aluno(nome, telefone, email);
+						Toast.makeText(FormularioAlunoActivity.this, alunoCriado.getNome() + " - " + alunoCriado.getTelefone() + " - " + alunoCriado.getEmail(), Toast.LENGTH_SHORT).show();
+					}
+				}
+		);
+	}
 }
