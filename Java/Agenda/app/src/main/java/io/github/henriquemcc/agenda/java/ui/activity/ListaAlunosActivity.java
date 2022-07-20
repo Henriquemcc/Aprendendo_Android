df6197aaa343bdd06e4dcc90f5786a0cbd762112
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-19 21:08:11 -0300

    Parte 7 da aula 4 do curso 'Android parte 1: crie um app mobile'.

--- Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/ui/activity/ListaAlunosActivity.java
+++ Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/ui/activity/ListaAlunosActivity.java
@@ -1,6 +1,5 @@
-package io.github.henriquemcc.agenda.java;
+package io.github.henriquemcc.agenda.java.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -10,20 +9,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
+import io.github.henriquemcc.agenda.java.R;
+
+public class ListaAlunosActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
 
         // Lista de alunos
         final List<String> alunos = Arrays.asList("João", "José", "Maria", "Carlos", "Gabriel", "Ana");
 
         // List View com os alunos
-        final ListView listViewListaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        final ListView listViewListaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
 
         // Adicionando adaptador com uma lista simples de itens
         listViewListaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
--- Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/ui/activity/ListaAlunosActivity.kt
+++ Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/ui/activity/ListaAlunosActivity.kt
@@ -1,26 +1,24 @@
-package io.github.henriquemcc.agenda.kotlin
+package io.github.henriquemcc.agenda.kotlin.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import io.github.henriquemcc.agenda.kotlin.R
 
-class MainActivity: AppCompatActivity()
+class ListaAlunosActivity: AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lista_alunos)
         title = "Lista de alunos"
 
         // Lista de alunos
         val alunos = listOf<String>("João", "José", "Maria", "Carlos", "Gabriel", "Ana")
 
         // List View com os alunos
-        val listViewListaDeAlunos = findViewById<ListView>(R.id.activity_main_lista_de_alunos)
+        val listViewListaDeAlunos = findViewById<ListView>(R.id.activity_lista_alunos_listview)
 
         // Adicionando adaptador com uma lista simples de itens
         listViewListaDeAlunos.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, alunos)
