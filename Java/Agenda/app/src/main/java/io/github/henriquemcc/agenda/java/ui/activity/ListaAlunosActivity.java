d0063360ffdc9937ba92be1d6a23f8a7865bdf49
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-20 19:15:15 -0300

    Parte 6 da aula 5 do curso 'Android parte 1: crie um app mobile' em Java.

--- Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/ui/activity/ListaAlunosActivity.java
+++ Java/Agenda/app/src/main/java/io/github/henriquemcc/agenda/java/ui/activity/ListaAlunosActivity.java
@@ -1,10 +1,15 @@
 package io.github.henriquemcc.agenda.java.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,17 +19,35 @@ import io.github.henriquemcc.agenda.java.dao.AlunoDAO;
 
 public class ListaAlunosActivity extends AppCompatActivity
 {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_alunos);
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lista_alunos);
 
-        final AlunoDAO dao = new AlunoDAO();
+		setTitle("Lista de alunos");
 
-        setTitle("Lista de alunos");
+		final FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+		botaoNovoAluno.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				startActivity(new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class));
+			}
+		});
 
-        final ListView listViewListaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
-        listViewListaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
-    }
+
+	}
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+
+		final AlunoDAO dao = new AlunoDAO();
+
+		final ListView listViewListaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+		listViewListaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
+	}
 }
