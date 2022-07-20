9579ed5538658aa181beca9a0115d36cfe953bde
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-20 16:54:39 -0300

    Parte 2 da aula 5 do curso 'Android parte 1: crie um app mobile' em Kotlin.

---
+++ Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/model/Aluno.kt
@@ -0,0 +1,3 @@
+package io.github.henriquemcc.agenda.kotlin.model
+
+class Aluno(val nome: String, val telefone: String, val email: String)
--- Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/ui/activity/FormularioAlunoActivity.kt
+++ Kotlin/Agenda/app/src/main/java/io/github/henriquemcc/agenda/kotlin/ui/activity/FormularioAlunoActivity.kt
@@ -2,11 +2,36 @@ package io.github.henriquemcc.agenda.kotlin.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import io.github.henriquemcc.agenda.kotlin.R
+import io.github.henriquemcc.agenda.kotlin.model.Aluno
 
-class FormularioAlunoActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_aluno)
-    }
+class FormularioAlunoActivity : AppCompatActivity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_formulario_aluno)
+
+		val campoNome = findViewById<EditText>(R.id.activity_formulario_aluno_nome)
+		val campoTelefone = findViewById<EditText>(R.id.activity_formulario_aluno_telefone)
+		val campoEmail = findViewById<EditText>(R.id.activity_formulario_aluno_email)
+
+		val botaoSalvar = findViewById<Button>(R.id.activity_formulario_aluno_botao_salvar)
+		botaoSalvar.setOnClickListener(object : View.OnClickListener
+		{
+			override fun onClick(p0: View?)
+			{
+				val nome = campoNome.text.toString()
+				val telefone = campoTelefone.text.toString()
+				val email = campoEmail.text.toString()
+
+				val alunoCriado = Aluno(nome, telefone, email)
+				Toast.makeText(this@FormularioAlunoActivity, "${alunoCriado.nome} - ${alunoCriado.telefone} - ${alunoCriado.email}", Toast.LENGTH_SHORT).show()
+			}
+		})
+	}
 }
