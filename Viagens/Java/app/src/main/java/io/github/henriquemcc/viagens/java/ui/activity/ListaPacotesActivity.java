1931cd233b32ec3e3881dd706e61ef0b3e4c885e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-01-18 17:44:13 -0300

    Parte 5 da aula 1 do curso "Layouts Android parte 1: criando telas com Constraint Layouts" em Java.

---
+++ Viagens/Java/app/src/main/java/io/github/henriquemcc/viagens/java/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,18 @@
+package io.github.henriquemcc.viagens.java.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.henriquemcc.viagens.java.R;
+
+public class ListaPacotesActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lista_pacotes);
+	}
+}
