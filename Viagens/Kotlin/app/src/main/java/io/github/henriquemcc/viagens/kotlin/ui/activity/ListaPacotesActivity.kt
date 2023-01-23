f1e7ff153ec895e880d4662e2270af87c4681063
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-01-23 20:15:40 -0300

    Parte 5 da aula 1 do curso "Layouts Android parte 1: criando telas com Constraint Layouts" em Kotlin.

---
+++ Viagens/Kotlin/app/src/main/java/io/github/henriquemcc/viagens/kotlin/ui/activity/ListaPacotesActivity.kt
@@ -0,0 +1,12 @@
+package io.github.henriquemcc.viagens.kotlin.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.henriquemcc.viagens.kotlin.R
+
+class ListaPacotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_pacotes)
+    }
+}
